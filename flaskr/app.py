b9e00d057b09da2d404e0c504d4923799e5c308f
Sihan <swang943@gatech.edu>
2023-04-01 18:45:18 -0400

    Update app.py

--- flaskr/app.py
+++ flaskr/app.py
@@ -14,8 +14,17 @@ amenities = uf.import_data("select * from wanderwisely.amenity_related_parks", c
 amenities = amenities["name"].unique()
 # load data to map parkCode to parkName
 parks_df = uf.import_data(f"select * from wanderwisely.activity_related_parks", conn)
+
+# load data to get lat/lon to selected places
+
+query = """select thing_title, lat, lon from wanderwisely.things_to_do_places as table1
+    inner join wanderwisely.activity_related_parks as table2
+    on table1.parkCode = table2.parkCode
+    where parkName = 'Acadia National Park' AND thing_title in ('Go Earthcaching At Acadia', 'Bike Carriage Roads') """
+loca = uf.import_data(query, conn)
+
 # record user's selection
-user_selection = {"activities": [], "amenities": [], "pois": []}
+user_selection = {"activities": [], "amenities": [], "parkName": [], "pois": []}
 
 
 def update_selection(selection, select_type):
@@ -41,7 +50,6 @@ def record_button():
 
     return '', 204
 
-
 @app.route('/parks')
 def parks():
     top_three_parks = algo(user_selection)
@@ -49,19 +57,21 @@ def parks():
 
 
 def generate_places(parkName, activities):
-    parkCode = parks_df[parks_df['parkName'] == parkName]['parkCode'][0]
+    parkCode = parks_df[parks_df['parkName'] == parkName]['parkCode'].tolist()[0]
+    activities = "','".join(activities)
     query = f"select thing_title from wanderwisely.things_to_do_places where parkCode = '{parkCode}' and activity_name in ('{activities}')"
     places_df = uf.import_data(query, conn)
     filtered_places = places_df['thing_title'].to_list()
-    return parkName, filtered_places
+    return filtered_places
 
 
 @app.route('/poi')
 def poi():
     parkName = user_selection['parkName']
     activities = user_selection['activities']
-    # parkName, places = generate_places(parkName, activities)
-    parkName, places = generate_places('acad', ['Biking', 'Hiking'])
+    places = generate_places(parkName, activities)
+    # parkName = 'Yosemite National Park'
+    # places = generate_places('Yosemite National Park', ['Hiking', 'Biking', 'Astronomy', 'Boating'])
     return render_template('poi.html', parkName=parkName, places=places)
 
 
@@ -78,4 +88,5 @@ def contact():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
