7765ca445437418c9d4dd366dbd05d17b4f07385
Liangqu Chen <lchenbusiness@gmail.com>
2023-04-01 18:44:57 -0400

    push

--- flaskr/app.py
+++ flaskr/app.py
@@ -15,14 +15,17 @@ amenities = amenities["name"].unique()
 # load data to map parkCode to parkName
 parks_df = uf.import_data(f"select * from wanderwisely.activity_related_parks", conn)
 # record user's selection
-user_selection = {"activities": [], "amenities": [], "pois": []}
+user_selection = {"activities": [], "amenities": [], "pois": [],"hours":[], "park":[]}
 
 
 def update_selection(selection, select_type):
-    if selection in user_selection[select_type]:
-        user_selection[select_type].remove(selection)
-    else:
-        user_selection[select_type].append(selection)
+    if select_type == "hours" or select_type == "park":
+            user_selection[select_type] = [selection]
+    else: # when select_type == amenities, activities, pois
+        if selection in user_selection[select_type]:
+            user_selection[select_type].remove(selection)
+        else:
+            user_selection[select_type].append(selection)
 
 
 app = Flask(__name__)
@@ -38,30 +41,33 @@ def record_button():
     data = request.get_json()
     update_selection(data["input"], data["type"])
     # Record the button click in the database or perform any other action
-
+    print(user_selection)
     return '', 204
 
 
 @app.route('/parks')
 def parks():
-    top_three_parks = algo(user_selection)
-    return render_template('parks.html',parks = top_three_parks)
+    top_three_parks = ["Acadia National Park", "Arches National Park", "Capitol Reef National Park"]
+    hours = [1,2,3,4,5,6,7,8,9,10,11,12]
+    return render_template('parks.html',parks = top_three_parks, hours = hours)
 
 
 def generate_places(parkName, activities):
-    parkCode = parks_df[parks_df['parkName'] == parkName]['parkCode'][0]
+    parkCode = parks_df[parks_df['parkName'] == parkName]['parkCode'].tolist()[0]
+    activities = "','".join(activities)
     query = f"select thing_title from wanderwisely.things_to_do_places where parkCode = '{parkCode}' and activity_name in ('{activities}')"
     places_df = uf.import_data(query, conn)
     filtered_places = places_df['thing_title'].to_list()
-    return parkName, filtered_places
+    return filtered_places
 
 
 @app.route('/poi')
 def poi():
-    parkName = user_selection['parkName']
+    parkName = user_selection['park'][0]
     activities = user_selection['activities']
-    # parkName, places = generate_places(parkName, activities)
-    parkName, places = generate_places('acad', ['Biking', 'Hiking'])
+    places = generate_places(parkName, activities)
+    # parkName = 'Yosemite National Park'
+    # places = generate_places('Yosemite National Park', ['Hiking', 'Biking', 'Astronomy', 'Boating'])
     return render_template('poi.html', parkName=parkName, places=places)
 
 
@@ -77,14 +83,6 @@ def contact():
 
 
 if __name__ == "__main__":
-<<<<<<< Updated upstream
     app.run(debug=True)
 
 
-
-
-
-=======
-
-    app.run(debug=True)
->>>>>>> Stashed changes
