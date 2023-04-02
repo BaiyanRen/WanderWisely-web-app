8ba068a3e047508d82325d571f6273845954c9b2
Liangqu Chen <lchenbusiness@gmail.com>
2023-04-02 15:45:56 -0400

    update get park.py
    update get park.py

--- flaskr/get_park.py
+++ flaskr/get_park.py
@@ -13,13 +13,12 @@ def get_park(amenity_names,activity_names):
         
         df_amenity['parkname_count'] = df_amenity.groupby('parkName')['name'].transform('count')
         df_amenties = df_amenity[['parkName', 'parkname_count']].drop_duplicates(subset='parkName').reset_index(drop=True)
-        
+
         df_activity['parkname_count'] = df_activity.groupby('parkName')['name'].transform('count')
         df_activities = df_activity[['parkName', 'parkname_count']].drop_duplicates(subset='parkName').reset_index(drop=True)
         df_merged = pd.merge(df_amenties, df_activities, on='parkName',how='outer').fillna(0)
-        df_merged.to_csv("../data/merged.csv',index=False)
         df_merged['parkname_count_total'] = df_merged['parkname_count_x'] + df_merged['parkname_count_y']
-        df_merged_sorted = df_merged.sort_values(by='parkname_count_total', ascending=False).head(3)
+        df_merged_sorted = df_merged.sort_values(by=['parkname_count_total','parkname_count_y'], ascending=False).head(3)
         data = df_merged_sorted['parkName'].values.tolist()
         return data
         
