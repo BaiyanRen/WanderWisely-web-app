3491915245c5896a959378c68ad16adf1d105cb1
Liangqu Chen <lchenbusiness@gmail.com>
2023-04-16 09:14:15 -0400

    add an error page to prevent missing user selection

--- flaskr/main.py
+++ flaskr/main.py
@@ -82,13 +82,17 @@ def record_button():
 
 @app.route('/parks')
 def parks():
-    # initiate parks in case users come back to the page
-    session['user_selection']["park"] = []
-    amenity_names=session['user_selection']['amenities']
-    activity_names=session['user_selection']['activities']
-    top_three_parks = gp.get_park(amenity_names,activity_names)
-    hours = [1,2,3,4,5,6,7,8,9,10,11,12]
-    return render_template('parks.html',parks = top_three_parks, hours = hours)
+    if len(session["user_selection"]["activities"]) == 0:
+        return errorPage("noActivity")
+
+    else:
+        # initiate parks in case users come back to the page
+        session['user_selection']["park"] = []
+        amenity_names=session['user_selection']['amenities']
+        activity_names=session['user_selection']['activities']
+        top_three_parks = gp.get_park(amenity_names,activity_names)
+        hours = [1,2,3,4,5,6,7,8,9,10,11,12]
+        return render_template('parks.html',parks = top_three_parks, hours = hours)
 
 
 def generate_places(parkName, activities):
@@ -106,74 +110,91 @@ def generate_places(parkName, activities):
     urls = places_df[["thing_title", "place_url", "image_url"]]
     return pois, urls
 
+@app.route('/errorPage')
+def errorPage(errorType):
+    if errorType=="noParkOrNoHour":
+        return render_template('errorPage.html', text="No park or hour is selected, Please go back and select ONE park or ONE hour",link = "/parks")
+    elif errorType=="lessThan2pois":
+        return render_template('errorPage.html', text="Please select at least two places",link = "/poi")
+    elif errorType=="noActivity":
+        return render_template('errorPage.html', text="Please select at least one activtity",link = "/ActivitiesAndAmenities")
 
 @app.route('/poi')
 def poi():
+    # check if park and hours are selected
+    if len(session['user_selection']["park"]) == 0 or len(session['user_selection']["hours"]) == 0:
+        return errorPage("noParkOrNoHour")
+
     # initiate pois in case users come back to the page
-    session['user_selection']["pois"] = []
-    parkName = session['user_selection']['park'][0]
-    activities = session['user_selection']['activities']
-    places, urls = generate_places(parkName, activities)
+
+    else:
+        session['user_selection']["pois"] = []
+        parkName = session['user_selection']['park'][0]
+        activities = session['user_selection']['activities']
+        places, urls = generate_places(parkName, activities)
     
-    return render_template('poi.html', parkName=parkName, places=places, urls = urls)
+        return render_template('poi.html', parkName=parkName, places=places, urls = urls)
 
 
 # get lat/lon of selected places
 @app.route('/generate_route')
 def generate_route():
-    print(session['user_selection'])
-    conn, engine = uf.conn_to_db()
-    query = """select distinct thing_title, lat, lon, duration from wanderwisely.things_to_do_places as table1
-    inner join wanderwisely.activity_related_parks as table2
-    on table1.parkCode = table2.parkCode
-    where parkName = '{}' AND thing_title in {} """ .format(session['user_selection']['park'][0], tuple(session['user_selection']['pois']))
-    loca = uf.import_data(query, conn)
+    if len(session["user_selection"]["pois"]) < 2:
+        return errorPage("lessThan2pois")
+    else:
+        print(session['user_selection'])
+        conn, engine = uf.conn_to_db()
+        query = """select distinct thing_title, lat, lon, duration from wanderwisely.things_to_do_places as table1
+        inner join wanderwisely.activity_related_parks as table2
+        on table1.parkCode = table2.parkCode
+        where parkName = '{}' AND thing_title in {} """ .format(session['user_selection']['park'][0], tuple(session['user_selection']['pois']))
+        loca = uf.import_data(query, conn)
 
 
 
-    # A = {"thing_title": "Hike Double Bubble Nubble Loop with Island Explorer", "lat":44.350011499069, 'lon':-68.2414535993951, "duration": 2.0}
-    # B = {"thing_title": "Hike Great Head Trail", "lat": 44.3300018310546, 'lon':-68.1775283813476, "duration": 4.0}
-    # C = {"thing_title": "Hike Ship Harbor Trail", "lat": 44.2284927368164, 'lon':-68.3237609863281, "duration": 1.0}
-    # D = {"thing_title": "Hike Giant Slide Loop", "lat": 44.35079167, 'lon':-68.30218833, "duration": 4.0}
-    # E = {"thing_title": "Hike Gorge Path", "lat": 44.372621, 'lon':-68.221942, "duration": 3.0}
-    # F = {"thing_title": "Hike Wonderland Trail", "lat": 44.23383331298821, 'lon':-68.3199996948242, "duration": 0.5}
-    # G = {"thing_title": "Hike Beachcroft Path", "lat": 44.3585023529493, 'lon':-68.2059851525353, "duration": 1.5}
-    # loca = pd.DataFrame([A,B,C,D,E,F,G])
+        # A = {"thing_title": "Hike Double Bubble Nubble Loop with Island Explorer", "lat":44.350011499069, 'lon':-68.2414535993951, "duration": 2.0}
+        # B = {"thing_title": "Hike Great Head Trail", "lat": 44.3300018310546, 'lon':-68.1775283813476, "duration": 4.0}
+        # C = {"thing_title": "Hike Ship Harbor Trail", "lat": 44.2284927368164, 'lon':-68.3237609863281, "duration": 1.0}
+        # D = {"thing_title": "Hike Giant Slide Loop", "lat": 44.35079167, 'lon':-68.30218833, "duration": 4.0}
+        # E = {"thing_title": "Hike Gorge Path", "lat": 44.372621, 'lon':-68.221942, "duration": 3.0}
+        # F = {"thing_title": "Hike Wonderland Trail", "lat": 44.23383331298821, 'lon':-68.3199996948242, "duration": 0.5}
+        # G = {"thing_title": "Hike Beachcroft Path", "lat": 44.3585023529493, 'lon':-68.2059851525353, "duration": 1.5}
+        # loca = pd.DataFrame([A,B,C,D,E,F,G])
 
-    # print(loca)
-    #get route
+        # print(loca)
+        #get route
 
-    locations, location_names, route_order, shortest_time, route_pair_distance, route_pair_time, duration, cal_time = tsp(loca)
-    total_time = round(shortest_time + sum(loca['duration']), 2)
+        locations, location_names, route_order, shortest_time, route_pair_distance, route_pair_time, duration, cal_time = tsp(loca)
+        total_time = round(shortest_time + sum(loca['duration']), 2)
 
     
 
-    # load images for places
-    route_order_sql = "','".join(route_order)
-    query = f"select thing_title, image_url, place_url from wanderwisely.things_to_do_places where thing_title in ('{route_order_sql}')"
+        # load images for places
+        route_order_sql = "','".join(route_order)
+        query = f"select thing_title, image_url, place_url from wanderwisely.things_to_do_places where thing_title in ('{route_order_sql}')"
 
 
-    route_data = uf.import_data(query, conn)
-    conn.close()
-    engine.dispose()
+        route_data = uf.import_data(query, conn)
+        conn.close()
+        engine.dispose()
 
-    route_data_dict = route_data.set_index('thing_title').to_dict()
-    days = ceil(total_time/float(session['user_selection']["hours"][0]))
+        route_data_dict = route_data.set_index('thing_title').to_dict()
+        days = ceil(total_time/float(session['user_selection']["hours"][0]))
 
-    #print("dictionary of route data: ", route_data_dict)
-    #print("shortest_path: ", route_order)
-    #print("total: ", total_time)
-    #print("route_pair_distance: ", route_pair_distance)
-    #print("route_pair_time: ", route_pair_time)
-    #print("duration: ", duration)
-    #print("cal time: ", cal_time)
-    #print("locations type: ", type(locations))
-    #print("locations: ", locations)
+        #print("dictionary of route data: ", route_data_dict)
+        #print("shortest_path: ", route_order)
+        #print("total: ", total_time)
+        #print("route_pair_distance: ", route_pair_distance)
+        #print("route_pair_time: ", route_pair_time)
+        #print("duration: ", duration)
+        #print("cal time: ", cal_time)
+        #print("locations type: ", type(locations))
+        #print("locations: ", locations)
 
 
-    return render_template('generate_route.html', park = session['user_selection']['park'][0], locations = locations, days = days, route_order = route_order,
-                           total_time = total_time, route_pair_distance = route_pair_distance,
-                           route_pair_time = route_pair_time, duration = duration, route_data_dict = route_data_dict)
+        return render_template('generate_route.html', park = session['user_selection']['park'][0], locations = locations, days = days, route_order = route_order,
+                               total_time = total_time, route_pair_distance = route_pair_distance,
+                               route_pair_time = route_pair_time, duration = duration, route_data_dict = route_data_dict)
 
 
 
